2f639bb5876944a1423433ffb66dca994fac3fd5
bilahari <bakkiraju@logitech.com>
2017-01-31 02:06:26 -0800

    multiscreen overlays added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-const electron = require('electron')
+const electron = require('electron');
+const net = require('net'), JsonSocket = require('json-socket');;
+
 const BrowserWindow = electron.BrowserWindow
 const ipcMain = electron.ipcMain
 const app = electron.app
@@ -6,163 +8,149 @@ const globalShortcut = electron.globalShortcut
 
 //const fs = require('fs')
 // const zmq = require('zeromq')
-
-var net = require('net');
-
 var HOST = '127.0.0.1';
 var PORT = 6969;
-
-
 // var responder = zmq.socket('rep')  // server for  connecting to external messages
 // var requester = zmq.socket('req')  // client that connects to external server
 
+electron.app.on('browser-window-created', function(e, window) {
+    window.setMenu(null);
+});
+
 app.on('ready', function() {
 
-  var prefsWindow;
-  /*Create various overlay windows based on payload */
-  overlayReqHandler = function(payload) {
-
-     opts = JSON.parse(payload).RenderingOptions;
-
-     console.log("Received data", opts);
-
-     if (opts.Command === "close") {
-       if (prefsWindow) {
-         if (prefsWindow.isVisible()){
-           prefsWindow.hide()
-           return;
-         }
-       } else {
-         return;
-       }
-     }
-
-     if (prefsWindow) {
-       if(!prefsWindow.isVisible()){
-         prefsWindow.show();
-         return;
-       }
-     }
-
-     prefsWindow = new BrowserWindow({
-        width  : opts.size.width,
-        height : opts.size.height,
-        show   : false,
-        frame  : false,
-        backgroundColor: '#ababab'
-     });
-
-     prefsWindow.loadURL('file://'+__dirname + '/../'+opts.RenderFile);
-
-     prefsWindow.webContents.send("update-scaffold",opts);
-
-     prefsWindow.on('focus', function(){
-        console.log("pref window showed up")
-
-        globalShortcut.register('Tab', function () {
-            prefsWindow.toggleDevTools();
-        });
-
-        // globalShortcut.register('Up', function () {
-        //     console.log("Pressed Up");
-        //     prefsWindow.webContents.send("update-value","Up")
-        // });
-        //
-        // globalShortcut.register('Down', function () {
-        //     console.log("Pressed Down");
-        //     prefsWindow.webContents.send("update-value","Down")
-        // });
-     });
-
-     prefsWindow.on('blur',function(){
-        console.log("pref window hidden");
-        globalShortcut.unregister("Up");
-        globalShortcut.unregister("Down");
-        globalShortcut.unregister("Tab");
-        console.log("Unregisterd key bindings");
-     });
-
-     globalShortcut.register('Cmd+Shift+Esc', function () {
-          console.log("Pref toggle hit");
-          if (prefsWindow.isVisible()) {
-             prefsWindow.setSize(0,0,true);
-             prefsWindow.hide();
-          } else {
-            if (prefBounds) {
-              console.log("Pref win bounds",prefBounds);
-              prefsWindow.setSize(prefBounds.width,prefBounds.height,true);
-              prefsWindow.show();
-            } else {
-              prefsWindow.show();
-              prefBounds = prefsWindow.getBounds();
-              console.log("Registered Pref bounds",prefBounds);
-            }
-            console.log("connecting to port 5556");
-            requester.connect("tcp://localhost:5556");
-         }
-      });
-      prefsWindow.show();
-      prefBounds = prefsWindow.getBounds();
-  };
-
-  net.createServer(function(sock) {
-      // We have a connection - a socket object is assigned to the connection automatically
-      console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-
-      sock.on('data',(data)=>{
-        overlayReqHandler(data)
-      });
-
-      sock.on('close', function(data) {
-          console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-      });
-
-  }).listen(PORT, HOST);
-
-  console.log('TCP Server listening on ' + HOST +':'+ PORT);
-
-  // responder.bind('tcp://*:5555', function(err) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log("0MQ socket listening on 5555…")
-  //   }
-  // });
-
   //  var circularPrefsWindow = new BrowserWindow({
+  //     name: "CirculatWindow",
   //      transparent:true,
   //      frame  : false,
-  //      width  : 350,
-  //      height : 350
-  //  })
-   //
-  //  circularPrefsWindow.loadURL('file://'+__dirname + '/circularPrefs.html')
-   //
-  //  circularPrefsWindow.show()
-
-  ipcMain.on("update-done", function(event,arg1,arg2){
-       console.log(arg1,arg2)
-
-       if (arg1 == "Up" || arg1 == "Down") {
-         console.log("Rendering update done via user interaction")
-         //requester.send(arg2)
-       } else {
-          console.log("Rendering update done via external message")
-          //responder.send(arg1)
-      }
-   });
-
-  //  responder.on('message', function(request) {
-  //    console.log("Received request: [", request.toString(), "]")
-  //    console.log("Sending to renderer")
-  //    prefsWindow.webContents.send("update-value",request.toString())
-  //  })
-
-  //  requester.on('message', function(arg){
-  //    console.log("Received ack from external entity:", arg.toString())
+  //      width  : 450,
+  //      height : 450
   //  });
+   //
+  //  circularPrefsWindow.loadURL('file://'+__dirname + '/../circularPrefs.html');
+   //
+  //  circularPrefsWindow.show();
+
+    var prefsWindow;
+    var opts;
+
+    prefsWindow = new BrowserWindow({
+        name: "OverlayWindow",
+        'node-integration': false,
+        transparent: true,
+        frame: false,
+        show: false,
+        width: 1000,
+        height: 300,
+        x:300,
+        y:900
+    });
+
+   prefsWindow.loadURL('file://' + __dirname + '/../prefs.html');
+
+    // prefsWindow.setAlwaysOnTop(true);
+    // prefsWindow.setIgnoreMouseEvents(true);
+
+
+    /*Create various overlay windows based on payload */
+    overlayReqHandler = function(payload) {
+
+       console.time('init');
+
+        opts = JSON.parse(payload).RenderingOptions;
+
+        console.log(new Date().toUTCString(), "Received data", opts);
+
+        if (opts.Command === "close") {
+            if (prefsWindow) {
+                if (prefsWindow.isVisible()) {
+                    prefsWindow.hide();
+                }
+            }
+            return;
+        }
+
+        if (opts.Command === "fade-out") {
+            prefsWindow.webContents.send("fade-out");
+            return;
+        }
+
+        if (opts.Command === "trava-tap") {
+            prefsWindow.webContents.send("trava-tap",opts);
+            return;
+        }
+
+        if (opts.Command === "update-overlay-value") {
+            prefsWindow.webContents.send("update-overlay-value",opts);
+            return;
+        }
+
+        prefsWindow.setSize(opts.size.width, opts.size.height);
+        prefsWindow.renderopts = opts;
+        prefsWindow.webContents.send("update-scaffold",opts);
+
+        // prefsWindow.once('ready-to-show', () => {
+        //    console.log(new Date().toUTCString(), "update scaffold sent");
+        //    prefsWindow.webContents.send("update-scaffold",opts);
+        //    prefsWindow.show()
+        // });
+
+        // prefsWindow.on('dom-ready', function() {
+        //    console.log("Web contents loaded");
+        // });
+
+        // prefsWindow.on('focus', function() {
+        //     console.log("pref window showed up")
+        //
+        //     globalShortcut.register('Tab', function() {
+        //         prefsWindow.toggleDevTools();
+        //     });
+        // });
+
+      //prefsWindow.show();
+
+      // prefsWindow.once('ready-to-show', () => {
+        prefsWindow.show()
+        console.timeEnd("init");
+      // });
+
+    };
+
+    net.createServer(function(sock) {
+        // We have a connection - a socket object is assigned to the connection automatically
+        console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+
+        sock.on('data', (data) => {
+          console.log("UI Engine Received json", new Date().toUTCString());
+          overlayReqHandler(data)
+        });
+
+        sock.on('close', function(data) {
+            console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+        });
+
+    }).listen(PORT, HOST);
+
+    console.log('TCP Server listening on ' + HOST + ':' + PORT);
+
+    ipcMain.on("scaffoling-loaded", function(event, arg) {
+       console.log("Scaffolding got loaded",arg);
+      //  prefsWindow.webContents.send("update-scaffold",opts);
+    });
+
+    ipcMain.on("update-done", function(event, arg1, arg2) {
+        console.log(arg1, arg2)
+
+        if (arg1 == "Up" || arg1 == "Down") {
+            console.log("Rendering update done via user interaction")
+            //requester.send(arg2)
+        } else {
+            console.log("Rendering update done via external message")
+            //responder.send(arg1)
+        }
+    });
 })
 
 process.on('SIGINT', function() {
-  //requester.close();
+    //requester.close();
 });
--- js/prefs.js
+++ js/prefs.js
@@ -3,66 +3,201 @@
 
 var React = require('react');
 var ReactDom = require('react-dom');
-
 var {ipcRenderer, remote} = require('electron');
+const root = document.getElementById('scaffold');
 
- const root = document.getElementById('scaffold');
+let RenderOpts = [];
 
-var MultiTabWidget =  React.createClass({
+function TabWidget(props) {
 
-    getInitialState: function() {
-        return {
-            opts: {}
-        };
-    },
+  const tabStyle = {
+       "fontSize": '50' + 'px',
+      "paddingTop": '20' + 'px'
+  };
 
-    componentDidMount: function(){
-      console.log("Multitab widget mounted")
-    },
+  console.log('Tab widget props', props.tabOpts);
 
-    render: function() {
-        return <div>Hello</div>
-    }
-});
+  const tabOpts = props.tabOpts;
 
-ReactDom.render(<MultiTabWidget/>, root);
-
-function prepareTab(tab, val) {
-   console.log(val);
+    return (
+        <div className="tab-item">
+          <div className={tabOpts.optSymbolType + " " + tabOpts.optSymbolValue}
+               style={tabStyle}>
+          </div>
+          <div id="ValueContainer">
+          </div>
+        </div>
+    );
 }
 
-ipcRenderer.on('update-scaffold',(event, arg) => {
-    console.log(arg);
+function SingleScreen(props) {
+
+  const tabStyle = {
+       "fontSize": '50' + 'px',
+      "paddingTop": '20' + 'px'
+  };
+
+  console.log('Single Screen widget props', props.tabOpts);
+
+  const tabOpts = props.tabOpts;
+
+    return (
+        <div>
+          <div className={tabOpts.optSymbolType + " " + tabOpts.optSymbolValue}
+               style={tabStyle}>
+          </div>
+          <div id="ValueContainer">
+            10
+          </div>
+        </div>
+    );
+}
+
+class MultiTabWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("MTW props",props);
+    };
+
+    render() {
+        const divStyle = {
+            'height': '100' + 'px'
+        };
+        var tabs = [];
+
+        if (this.props.opts.ToolOptions) {
+          //bakkiraju: index shouldnt be used, this is for PoC only
+            this.props.opts.ToolOptions.map((tabOpts,index) => {
+                tabs.push(<TabWidget key={index} tabOpts={tabOpts}/>);
+            });
+        }
+
+        return (
+          <div>
+            <div className="tab-group" style={divStyle}>
+                {tabs}
+            </div>
+          </div>
+        );
+    }
+}
+
+class MultiScreenWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("MSW props",props);
+    };
+
+    render() {
+        var tabs = [];
+
+        if (this.props.opts.ToolOptions) {
+          //bakkiraju: index shouldnt be used, this is for PoC only
+          this.props.opts.ToolOptions.map((tabOpts,index) => {
+              tabs.push(<SingleScreen key={index} tabOpts={tabOpts}/>);
+          });
+        }
+
+        return (
+          <div className="flipster">
+              <ul>
+                {tabs}
+              </ul>
+          </div>
+        );
+    }
+}
+
+class CircularWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("CW props",props);
+    };
+
+    render() {
+        return (
+          <div>
+            <div id="ValueContainer">
+                15
+            </div>
+          </div>
+        );
+    }
+}
+
+
+ipcRenderer.on('fade-out',(event, arg) =>{
+  console.log("changing to fade-out css class");
+  $("body").attr('class','app1');
+  $("#scaffold").removeClass();
+  var x = document.querySelector(".app1");
+  x.addEventListener("animationend", (e)=>{
+    remote.getCurrentWindow().hide();
+  }, false);
 });
 
-// function showTabContent(id) {
-//   //activeTab = document.getElementsByClassName('.tab-item active');
-//    console.log(id);
-// }
-//
-// function updateValue(arg) {
-//
-//   var elem = document.getElementById('active_tab_content')
-//
-//   var val = parseInt(elem.innerHTML);
-//
-//   if (arg === "Up") {
-//     console.log("Increasing value");
-//     val = val + 1
-//   } else if (arg === "Down") {
-//     console.log("Decreasing value")
-//     val = val - 1
-//   } else {
-//     val = arg;
-//     console.log("Updated via external message:",val)
-//   }
-//
-//   elem.innerHTML = val.toString();
-//
-//   ipcRenderer.send("update-done",arg,val)
-// }
-//
-// ipcRenderer.on('update-value',(event, arg) => {
-//     console.log(arg);
-//     updateValue(arg);
-// });
+ipcRenderer.on('update-scaffold', (event, arg) => {
+    const widgetStyle = remote.getCurrentWindow().renderopts.RenderStyle;
+    if ( widgetStyle === "Circular") {
+      $("body").removeClass('app');
+      console.log(new Date().toUTCString(),"Setting to circular class");
+      $("#scaffold").attr('class','circle');
+      ReactDom.render(
+          <CircularWidget opts={remote.getCurrentWindow().renderopts}/>,
+          root);
+    } else if (widgetStyle === "MultiScreen") {
+      console.log(new Date().toUTCString(),"Setting to Multi Screen class");
+      // $("body").attr('class','app');
+      // ReactDom.render(
+      //     <MultiScreenWidget opts={remote.getCurrentWindow().renderopts}/>,
+      //     root);
+      //
+      // $('.flipster').flipster({
+      //           style: 'carousel'
+      //  });
+    } else {
+      console.log(new Date().toUTCString(),"Setting to normal class");
+      $("body").attr('class','app');
+      $("#scaffold").removeClass();
+      ReactDom.render(
+          <MultiTabWidget opts={remote.getCurrentWindow().renderopts}/>,
+          root);
+    }
+})
+
+
+ipcRenderer.on('trava-tap', (event, args)=> {
+   console.log("trava-tap", args);
+   
+})
+
+ipcRenderer.on('update-overlay-value', (event, args)=> {
+   console.log("update-overlay-value", args);
+   var elem = $(".flipster__item--current").find("#val");
+   elem.html(args.Value);
+})
+
+ipcRenderer.on('tapped', (event, arg) => {
+    const widgetStyle = remote.getCurrentWindow().renderopts.RenderStyle;
+    if (widgetStyle === "Circular") {
+      $("body").removeClass('app');
+      console.log(new Date().toUTCString(),"Setting to circular class");
+      $("#scaffold").attr('class','circle');
+      ReactDom.render(
+          <CircularWidget opts={remote.getCurrentWindow().renderopts}/>,
+          root);
+    } else if (widgetStyle === "Multiscreen")
+    {
+      $("body").attr('class','app');
+      ReactDom.render(
+          <MultiScreenWidget opts={remote.getCurrentWindow().renderopts}/>,
+          root);
+    } else {
+      console.log(new Date().toUTCString(),"Setting to normal class");
+      $("body").attr('class','app');
+      $("#scaffold").removeClass();
+      ReactDom.render(
+          <MultiTabWidget opts={remote.getCurrentWindow().renderopts}/>,
+          root);
+    }
+})
